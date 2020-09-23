6b324ffd22e978852b92ea831475f413efa43d40
Ajay Jose <ajaypjose@hotmail.com>
2020-09-23 14:52:21 -0700

    Implemented the Read method inside the File Reader class.

--- WordUnscrambler/FileReader.cs
+++ WordUnscrambler/FileReader.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.IO;
 namespace WordUnscrambler
 {
     class FileReader
@@ -12,10 +12,21 @@ namespace WordUnscrambler
         {
             //declare a string[] to hold the contents of the file 
             //try/catch
-            
+
             //read from the file - ReadAllLines()
             //return file contents, which is a string[];
-            
+            string[] readFiles;
+
+            try
+            {
+                readFiles = File.ReadAllLines(fileName);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+            return readFiles;
         }
     }
 }
